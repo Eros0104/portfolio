57e2ca939244f8a9bcd7ad731d95bd2a57b4bf9e
Eros0104 <erosmendonca@outlook.com>
2022-04-07 00:09:12 -0300

    feat: fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,11 @@ import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import Layout from '../components/main';
 import theme from '../styles/theme';
+import Fonts from '../styles/fonts';
 
 const MyApp = ({ Component, pageProps, router }: AppProps) => (
   <ChakraProvider theme={theme}>
+    <Fonts />
     <Layout router={router}>
       <Component {...pageProps} key={router.route} />
     </Layout>
