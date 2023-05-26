d00556490a06eca11e2059349a12257a2575f3a5
Eros Mendonça <erosmendonca@outlook.com>
2023-05-26 11:47:54 +0100

    chore: installing vercel analytics tool 📊

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,17 +4,21 @@ import { ChakraProvider } from '@chakra-ui/react';
 import Layout from '../components/main';
 import theme from '../styles/theme';
 import Fonts from '../styles/fonts';
+import { Analytics } from '@vercel/analytics/react';
 
 const MyApp = ({ Component, pageProps, router }: AppProps) => (
-  <ChakraProvider theme={theme}>
-    <Fonts />
-    <Layout router={router}>
-      {
-        // @ts-ignore
-        <Component {...pageProps} key={router.route} />
-      }
-    </Layout>
-  </ChakraProvider>
+  <>
+    <ChakraProvider theme={theme}>
+      <Fonts />
+      <Layout router={router}>
+        {
+          // @ts-ignore
+          <Component {...pageProps} key={router.route} />
+        }
+      </Layout>
+    </ChakraProvider>
+    <Analytics />
+  </>
 );
 
 export default MyApp;
